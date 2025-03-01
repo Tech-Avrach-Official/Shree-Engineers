06e9a89c616080061f05351020c77b9afa1beec0
Tech-Avrach <nikhilpatel9226@gmail.com>
2025-03-01 15:30:54 +0530

    refactor: update file permissions and integrate Tailwind CSS into the project

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='bg-green-300'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis omnis consectetur dicta voluptatum ut culpa dolorum, a eaque nihil, quas quidem reprehenderit ad ipsum nemo, illum tenetur. Sit inventore voluptatem dolores aspernatur totam dignissimos tenetur? Quisquam optio, quaerat ullam modi illo provident ipsum vero ex?</div>
     </>
   )
 }
