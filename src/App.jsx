96899c5d626101ecbd8bc6f3c08828abec8879fa
Tech-Avrach <nikhilpatel9226@gmail.com>
2025-03-01 18:02:18 +0530

    feat: add Tailwind CSS configuration and utility components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,8 @@ function App() {
   return (
     <>
       <div className='bg-green-300'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis omnis consectetur dicta voluptatum ut culpa dolorum, a eaque nihil, quas quidem reprehenderit ad ipsum nemo, illum tenetur. Sit inventore voluptatem dolores aspernatur totam dignissimos tenetur? Quisquam optio, quaerat ullam modi illo provident ipsum vero ex?</div>
+
+      <Button>CLick me</Button>
     </>
   )
 }
